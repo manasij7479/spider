cd20b138b8f0049d6e57777ac8924274fcaff3b1
manasij7479 <manasij7479@gmail.com>
2014-09-03 03:24:27 +0530

    Functionality for generating simple C++ code

--- main.cpp
+++ main.cpp
@@ -1,28 +1,8 @@
-#include <ogdf/basic/Graph.h>
-#include <ogdf/basic/graph_generators.h>
-#include <ogdf/energybased/FMMMLayout.h>
- 
-using namespace ogdf;
+#include <iostream>
+#include "CppGen.hpp"
  
 int main()
 {
-//     Graph G;
-//     GraphAttributes GA(G);
-// 
-//     randomSimpleGraph(G,10,20);
-//     
-//     node v;
-//     forall_nodes(v,G)
-//             GA.width(v) = GA.height(v) = 10.0;
-// 
-//     FMMMLayout fmmm;
-// 
-//     fmmm.useHighLevelOptions(true);
-//     fmmm.unitEdgeLength(15.0); 
-//     fmmm.newInitialPlacement(true);
-//     fmmm.qualityVersusSpeed(FMMMLayout::qvsGorgeousAndEfficient);
-// 
-//     fmmm.call(GA);
-//     GA.writeSVG("a.svg");
-//     return 0;
+    std::cout<<spider::cpp::WhileStmt(new spider::cpp::Literal("true"),new spider::cpp::Stmt()).str();
+    
 }
