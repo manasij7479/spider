0ef338168a3299847a1e5b99b50c55645057db4a
manasij7479 <manasij7479@gmail.com>
2014-09-02 23:08:06 +0530

    Blank Main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 #include <ogdf/basic/Graph.h>
 #include <ogdf/basic/graph_generators.h>
-#include <ogdf/layered/DfsAcyclicSubgraph.h>
+#include <ogdf/energybased/FMMMLayout.h>
  
 using namespace ogdf;
  
 int main()
 {
-        Graph G;
-        randomSimpleGraph(G, 10, 20);
-        DfsAcyclicSubgraph DAS;
-        DAS.callAndReverse(G);
-        G.writeGML("test.gml");
-        return 0;
+//     Graph G;
+//     GraphAttributes GA(G);
+// 
+//     randomSimpleGraph(G,10,20);
+//     
+//     node v;
+//     forall_nodes(v,G)
+//             GA.width(v) = GA.height(v) = 10.0;
+// 
+//     FMMMLayout fmmm;
+// 
+//     fmmm.useHighLevelOptions(true);
+//     fmmm.unitEdgeLength(15.0); 
+//     fmmm.newInitialPlacement(true);
+//     fmmm.qualityVersusSpeed(FMMMLayout::qvsGorgeousAndEfficient);
+// 
+//     fmmm.call(GA);
+//     GA.writeSVG("a.svg");
+//     return 0;
 }
-
