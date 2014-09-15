3c24cb918d704097029fdbd536938660b84bd0e5
manasij7479 <manasij7479@gmail.com>
2014-09-16 01:31:08 +0530

    cleaned up main

--- main.cpp
+++ main.cpp
@@ -1,25 +1,6 @@
-
- 
- 
+//This is to be currently used for testing pusposes
+#include <iostream>
 int main()
 {
-//     Graph G;
-//     GraphAttributes GA(G);
-// 
-//     randomSimpleGraph(G,10,20);
-//     
-//     node v;
-//     forall_nodes(v,G)
-//             GA.width(v) = GA.height(v) = 10.0;
-// 
-//     FMMMLayout fmmm;
-// 
-//     fmmm.useHighLevelOptions(true);
-//     fmmm.unitEdgeLength(15.0); 
-//     fmmm.newInitialPlacement(true);
-//     fmmm.qualityVersusSpeed(FMMMLayout::qvsGorgeousAndEfficient);
-// 
-//     fmmm.call(GA);
-//     GA.writeSVG("a.svg");
-//     return 0;
+    std::cout<<"Hello";
 }
