9d7ca1d56abce4130cbd997629dc272239f85704
manasij7479 <manasij7479@gmail.com>
2015-01-04 05:21:06 +0530

    Call XInitThreads

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include <sstream>
 #include "Event/EventManager.hpp"
 #include "Runtime/Runtime.hpp"
+#include "X11/Xlib.h"
 int main()
 {
-    
+    if (XInitThreads() == 0)
+    {
+        std::cerr<<"Threading not fully supported, do not open more than one window.\n";
+    }
     spider::EventManager eMgr;
 
     spider::Runtime rt(&eMgr);
