ce8c04f40486db299a0e695d5819f33c7bf3ed28
manasij7479 <manasij7479@gmail.com>
2015-01-07 19:55:17 +0530

    Disable OGDF adapter

--- include/External/OGDFAdapter.hpp
+++ include/External/OGDFAdapter.hpp
@@ -1,17 +1,17 @@
-#include <ogdf/basic/Graph_d.h>
-#include "../graph/graph.hpp"
-#include "../graph/algorithm/collections.hpp"
-namespace spider
-{
-    ogdf::Graph ConvertToOGDF(graph::Graph<int,bool>& g)
-    {
-        ogdf::Graph result;
-        std::map<int, ogdf::node> nodemap;
-        for (auto v : graph::VertexList(g))
-            nodemap[v] = result.newNode(v);
-        for (auto e : graph::EdgeList(g))
-            result.newEdge(nodemap[std::get<0>(e)],nodemap[std::get<1>(e)]);
-        return result;
-    }
-
-}
+// #include <ogdf/basic/Graph_d.h>
+// #include "../graph/graph.hpp"
+// #include "../graph/algorithm/collections.hpp"
+// namespace spider
+// {
+//     ogdf::Graph ConvertToOGDF(graph::Graph<int,bool>& g)
+//     {
+//         ogdf::Graph result;
+//         std::map<int, ogdf::node> nodemap;
+//         for (auto v : graph::VertexList(g))
+//             nodemap[v] = result.newNode(v);
+//         for (auto e : graph::EdgeList(g))
+//             result.newEdge(nodemap[std::get<0>(e)],nodemap[std::get<1>(e)]);
+//         return result;
+//     }
+// 
+// }
