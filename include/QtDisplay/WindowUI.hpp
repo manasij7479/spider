599a16e8833d5e9f97d0db61ac38a0bb6f1d2e32
SBMama <subhayan.rocks@gmail.com>
2015-04-11 13:33:02 +0530

    Documentation

--- include/QtDisplay/WindowUI.hpp
+++ include/QtDisplay/WindowUI.hpp
@@ -9,21 +9,60 @@ class QGraphicsView;
 class QGraphicsScene;
 class QMouseEvent;
 class QEvent;
+/** \brief - to display all the different components of visualization window.
+ * **/
 namespace spider
 {
+  
     class LayoutPainter;
+    /**
+     * \brief - Class to control and display the different components of visualization window.
+     * 
+     * The different components consists of the graph displaying componenet, tool bar and menu bar.
+     * The tool bar supports functions like zoom in, zoom out, rotate left, rotate right, fit to page.
+     * **/
     class WindowUI : public QWidget
     {
         Q_OBJECT
     public:
+	/** \brief - Constructor
+	 * 
+	 * @param GraphValue* gWrap - First Parameter, a GraphValue object. Contains different graph attributes.
+	 * 
+	 * @param Layout* l - Second Parameter, the layout in which the graph is to be displayed.
+	 **/
         WindowUI(GraphValue* gWrap, Layout* l);
+	/** \brief - function to change layout of the displayed graph
+	 *
+	 * @param Layout* newLayout - Parameter, the new layout in which the graph is to be displayed.
+	 **/
         void changeLayout(Layout* newLayout);
+	/**
+	 * \brief - function to check if the window is open.
+	 * Returns true if window is open, false otherwise.
+	 * **/
         bool isOpen(){return true;}
+        /** \brief - function to get a graph
+	 *
+	 * @returns GraphValue* - attributes of the graph.
+	 **/
         GraphValue* getGraph()
         {
             return g;
         }
+        /**
+	 * \brief - to filter events.
+	 * 
+	 * @param QObject* obj - First Parameter, a Qt object
+	 * 
+	 * @param QEvent* event - Second Parameter, a Qt event.
+	 * **/
         bool eventFilter(QObject *obj, QEvent *event);
+	/**
+	 * \brief - a function to get Layout getLayoutPainter
+	 * 
+	 * @returns LayoutPainter* - attributes of Layout Painter.
+	 * **/
         LayoutPainter* getLayoutPainter(){return lp;}
     private slots:
         void change();
